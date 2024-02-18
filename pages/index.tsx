ee6aeb6676d67d14173cd1ae1c26e0d485b2711e
maemae9312 <78950304+maemae9312@users.noreply.github.com>
2024-02-18 19:27:15 +0900

    initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,56 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect, useState } from "react";
+import styles from "./index.module.css";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
-  </Layout>
-);
+// getServerSidePropsから渡されるPropsの型
+type Props = {
+    initialImageUrl: string;
+};
 
+
+const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
+    const [imageUrl,setImageUrl] = useState(initialImageUrl);
+    const [loading, setLoading] = useState(false);
+    // useEffect(() => {
+    //     fetchImage().then((newImage) => {
+    //         setImageUrl(newImage.url);
+    //         setLoading(false);
+    //     });
+    // }, []);
+    const handleClick = async () => {
+        setLoading(true); //読み込み中フラグ
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url)  //画像URLの状態を更新
+        setLoading(false); // 読み込み中フラグ
+    };
+    return (
+        <div className={styles.page}>
+            <button onClick={handleClick} className= {styles.button}>
+                他のにゃんこも見る
+            </button>
+            <div className={styles.frame}>{
+                loading || <img src={imageUrl} className={styles.img} />}
+            </div>
+        </div>
+    )
+};
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+    const image = await fetchImage();
+    return {
+        props: {
+            initialImageUrl: image.url,
+        },
+    };
+};
+
+type Image = {
+    url: string;
+};
+const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+};
